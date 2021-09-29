9095cfb308a4e0dfb4bffef1455a473180451cb2
Grace <grace1@csu.fullerton.edu>
2021-09-28 19:30:05 -0700

    done

--- part-2/pattern.cc
+++ part-2/pattern.cc
@@ -13,32 +13,20 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  // TODO: Declare a const int named kCounterMax and initialize it to 22.
-const int kCounterMax = 22;
-  // TODO: Write an outer loop which starts from 0 and goes up to the counter
-  // max.
-for(int for_counter = 0; for_counter < kCounterMax; for_counter++){
+  const int kCounterMax = 22;
 
-  for(int second_counter = 0; second_counter < for_counter; second_counter++){
+  for (int for_counter = 0; for_counter < kCounterMax; for_counter++) {
+    for (int second_counter = 0; second_counter < for_counter;
+         second_counter++) {
+      cout << "-";
+    }
     cout << "*";
+    for (int third_counter = for_counter; third_counter < kCounterMax;
+         third_counter++) {
+      cout << "|";
+    }
+    cout << endl;
   }
-for(int third_counter = for_counter; third_counter< kCounterMax;third_counter++){
-  cout << "|";
-}
-cout << endl;
-
-  }
-
-  // TODO: Write an inner loop which starts from the current line number and
-  // counts down to zero. Make sure that this loop is inside the outer loop.
-
-  // TODO: Print an asterisk.
-
-  // TODO: Write another inner loop which starts from the current line number
-  // and counts up to the counter max. Make sure that this loop is inside the
-  // outer loop but outside the first inner loop.
-
-  // TODO: print a new line character
 
   return 0;
 }
